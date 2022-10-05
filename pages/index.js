4da7ad38aaffd34aed70565dd235b1db8a03936e
Lan-ZhiXi <1327901843@qq.com>
2022-10-05 16:33:53 +0800

    Navigating Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 const Home = () => {
+    const router = useRouter()
+
+    const handleClick1 = () => {
+        console.log('Placing your order')
+        router.push('/product')
+    }
+    const handleClick2 = () => {
+        console.log('Placing your order')
+        router.replace('/product')
+    }
     return (
         <div>
             <h1>Home Page</h1>
             <Link href='/blog'>Blog</Link>
             <br />
             <Link href='/product'>Products</Link>
+            <button onClick={() => { handleClick1() }}>
+                Place Order-push
+            </button>
+            <button onClick={() => { handleClick2() }}>
+                Place Order-replace
+            </button>
         </div>
     )
 }
